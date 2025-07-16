e81ee935fd8ec93a8c4fcf2b08c4464b6ec7e9a7
xvmdvx <121970707+xvmdvx@users.noreply.github.com>
2025-07-16 11:20:14 -0600

    fix order search count in trial floater

--- environments/db/db_launcher.js
+++ environments/db/db_launcher.js
@@ -661,9 +661,7 @@ class DBLauncher extends Launcher {
                             if (client.email) parts.push(`"${client.email}"`);
                             if (client.name) parts.push(`"${client.name}"`);
                             if (parts.length && client.email) {
-                                const base = 'https://db.incfile.com/order-tracker/orders/order-search';
-                                const url = base + '?fennec_email=' + encodeURIComponent(client.email);
-                                bg.openActiveTab({ url });
+                                bg.send('focusDbSearch', { email: client.email });
                             } else if (parts.length) {
                                 const query = parts.map(p => encodeURIComponent(p)).join('+OR+');
                                 const url = 'https://mail.google.com/mail/u/0/#search/' + query;
