b0af188b0a8a91fe46561d3082f5cdffee00db89
xvmdvx <121970707+xvmdvx@users.noreply.github.com>
2025-07-17 12:01:59 -0600

    fix: keep focus when opening Ekata

--- environments/kount/kount_launcher.js
+++ environments/kount/kount_launcher.js
@@ -102,9 +102,7 @@ class KountLauncher extends Launcher {
                         chrome.storage.local.get({ fennecFraudAdyen: null }, ({ fennecFraudAdyen }) => {
                             if (fennecFraudAdyen) {
                                 chrome.storage.local.remove('fennecFraudAdyen');
-                                bg.openOrReuseTab({ url: fennecFraudAdyen, active: true }, () => {
-                                    bg.refocusTab();
-                                });
+                                bg.openOrReuseTab({ url: fennecFraudAdyen, active: true });
                             } else {
                                 bg.refocusTab();
                             }
