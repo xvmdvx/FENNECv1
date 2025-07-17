e5b4e340c8677d7dfb3a780de812097945798513
xvmdvx <121970707+xvmdvx@users.noreply.github.com>
2025-07-17 16:21:17 -0600

    Suppress DataTables AJAX warnings

--- environments/db/datatables_patch.js
+++ environments/db/datatables_patch.js
@@ -4,13 +4,18 @@
     function setErrMode(){
         var jq = window.jQuery || window.$;
         if (jq && jq.fn && jq.fn.dataTable && jq.fn.dataTable.ext){
-            jq.fn.dataTable.ext.errMode = 'none';
+            jq.fn.dataTable.ext.errMode = function(){ /* no-op */ };
+            jq(document).on('error.dt', function(e){
+                e.preventDefault();
+            });
             return true;
         }
         return false;
     }
+
     function apply(){
         if (!setErrMode()) setTimeout(apply, 50);
     }
+
     apply();
 })();
